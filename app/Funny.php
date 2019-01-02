b4f5dc3fc86bb410340298a5f3838647dca6c3b4
meow <sharmainemanijado@gmail.com>
2019-01-02 09:55:38 +0800

    Add more jokes

--- app/Funny.php
+++ app/Funny.php
@@ -12,14 +12,64 @@ class Funny extends Model
         return collect([
             [
                 'id'        => 0,
-                'question'  => 'Which super hero is the most curious?',
-                'answer'    =>  'Wonder woman'
+                'question'  => 'Sinong superhero ang mukhang pwet?',
+                'answer'    => 'Edi si BUTTman (batman) 😂'
             ],
             [
                 'id'        =>  1,
                 'question'  => 'Anong thor ang paboritong pagkain ni Thor?',
-                'answer'    =>  'Edi Thortang talong'
-            ]
+                'answer'    =>  'Edi Thortang talong :p'
+            ],
+            [
+                'id'        =>  3,
+                'question'  => 'Sinong superhero ang nandyadyan?',
+                'answer'    => 'Edi si There!devil (Daredevil) 😂'
+            ],
+            [
+                'id'        =>  4,
+                'question'  => 'Sinong superhero ang nabulok na?',
+                'answer'    => 'Edi si Na PANISher (The Punisher) 😂'
+            ],
+            [
+                'id'        =>  5,
+                'question'  => 'Sinong Superhero ang bulok?',
+                'answer'    => 'Edi si The incrediBULOK (The Incredible Hulk) 😂'
+            ],
+            [
+                'id'        =>  6,
+                'question'  => 'Sinong superhero ang nagcocommute?',
+                'answer'    => 'Edi si BUS Lightyear (Buzz Lightyear) 😂'
+            ],
+            [
+                'id'        =>  7,
+                'question'  => 'Sinong superhero ang mayroong maraming mata?',
+                'answer'    => 'Edi si EYES-man (Iceman) 😂'
+            ],
+            [
+                'id'        =>  8,
+                'question'  => 'Sinong superhero ang may-ari ng isang store?',
+                'answer'    => 'Edi si Store-m (Storm) 😂'
+            ],
+            [
+                'id'        =>  9,
+                'question'  => 'Sinong superhero ang mahilig sa sapatos?',
+                'answer'    => 'Edi si Shoeperman (Superman) :p'
+            ],
+            [
+                'id'        =>  10,
+                'question'  => 'Sinong superhero ang sobrang taba?',
+                'answer'    => 'Edi si Fatman (Batman) 😂'
+            ],
+            [
+                'id'        =>  11,
+                'question'  => 'Sinong superhero ang mahilig sa caffeine?',
+                'answer'    => 'Edi si Caffeine Varvel!! (Captain Barbel) 😂'
+            ],
+            [
+                'id'        =>  12,
+                'question'  => 'Bakit S ang nasa damit ni superman..?',
+                'answer'    => 'Kasi walang medium 🤣'
+            ],
         ]);
     }
 }
--- routes/botman.php
+++ routes/botman.php
@@ -6,20 +6,35 @@ use BotMan\Drivers\Facebook\Extensions\ButtonTemplate;
 use Illuminate\Support\Facades\Log;
 use BotMan\Drivers\Facebook\FacebookDriver;
 use App\Conversations\JokeConversation;
+use BotMan\Drivers\Facebook\Extensions\MediaTemplate;
+use BotMan\Drivers\Facebook\Extensions\MediaUrlElement;
+use BotMan\Drivers\Facebook\Extensions\MediaAttachmentElement;
 
 $botman = resolve('botman');
 
 $botman->hears('GET_STARTED', function (BotMan $bot) {
-    $bot->reply('Hello! My name is Ram.');
+    $user = $bot->getUser();
+
+    $bot->reply('Hello '. $user->getFirstName() .'! Welcome to Geekery. My name is Ram.');
+
     $bot->typesAndWaits(3);
     $bot->reply(
-        ButtonTemplate::create('May joke ako')
+        ButtonTemplate::create('Gusto mo ng joke? :D')
         ->addButton(
-            ElementButton::create('Ano')
+            ElementButton::create('G!')
             ->type('postback')
             ->payload('ano')
+        )
+        ->addButton(
+            ElementButton::create(('Nope, may ask lang me'))
+            ->type('postback')
+            ->payload('notnow')
         )
     );
 });
 
 $botman->hears('ano', BotManController::class.'@startConversation');
+
+$botman->hears('notnow', function (BotMan $bot) {
+    $bot->reply('Happy to serve :) What can I do for you?');
+});
